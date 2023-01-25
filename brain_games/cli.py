ffd3938eada370a8865913963852ec1e902cc625
Your Name <mesto3@gmailcom>
2023-01-25 17:31:13 +0700

    edit code-style

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,12 +2,15 @@
 
 import prompt
 
+
 def welcome_user(name):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
 
+
 def main():
     welcome_user('')
+
 
 if __name__ == '__main__':
     main()
