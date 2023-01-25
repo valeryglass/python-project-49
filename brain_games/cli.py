e6a0fc9df9753c7b77b8c09b4514820944326c10
Your Name <mesto3@gmailcom>
2023-01-25 15:45:50 +0700

    add CLI logic and update version to 0.2.0

---
+++ brain_games/cli.py
@@ -0,0 +1,13 @@
+# file <cli.py> CLI logic
+
+import prompt
+
+def welcome_user(name):
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+
+def main():
+    welcome_user('')
+
+if __name__ == '__main__':
+    main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+# file <brain_games.py> running script
+
+from brain_games.cli import welcome_user
 
 def main():
     print(f'Welcome to the Brain Games!')
+    welcome_user('')
 
 if __name__ == '__main__':
     main()
-    
+
