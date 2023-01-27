26e7d4bd43be87b0ef1a844b54db94ab1075ff05
Your Name <mesto3@gmailcom>
2023-01-27 21:09:47 +0700

    Modify brain-even logic

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,16 +2,17 @@
 # file <brain_even.py> brain_even game script
 
 import random
-#from brain_games.cli import welcome_user
+# from brain_games.cli import welcome_user
 
-#welcome_user('')
+# welcome_user('')
 
 print('Welcome to Brain Games!')
 name = input('May I have your name? ')
 print(f'Hello, {name}!')
 print('Answer "yes" if the number is even, otherwise answer "no".')
 
-def main():        
+
+def main():
     i = 1
     corr_ans = ''
     num = ''
@@ -20,7 +21,7 @@ def main():
     # счетчик правильных игр
     while i < round_count:
         # генерация и определения правильного результата
-        num = random.randint(1,9)
+        num = random.randint(1, 9)
         if (num % 2) == 0:
             corr_ans = 'yes'
         else:
@@ -31,7 +32,6 @@ def main():
             print(f"'{ans}' is not a answer ;(. Lets`s try again, {name}!")
             break
 
-
     # сравниваем с ответом пользователя
         if ans == corr_ans:
             i = i + 1
@@ -40,10 +40,10 @@ def main():
             print(f"'{ans}' is wrong answer ;(. Correct answer was '{corr_ans}'. Lets`s try again, {name}!")
             break
 
-
-    # поздравляем если победитель    
+    # поздравляем если победитель
     if i >= round_count:
         print(f'Congratulations, {name}!')
+
 
 if __name__ == '__main__':
     main()
