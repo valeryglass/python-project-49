08fb21502d3b8783bfc8718367795543d3fd71ec
Your Name <mesto3@gmailcom>
2023-01-31 20:35:47 +0700

    Add game-summary to CLI

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,10 +2,25 @@
 
 import prompt
 
+# welcoming and KYC
+
 
 def welcome_user(name):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
+    return name
+
+
+# calculating game summary
+
+
+def summary(i, round_count, name):
+    if i >= round_count:
+        summary_text = print(f'Congratulations, {name}!')
+        return summary_text    
+    else:
+        summary_text = print(f"Let's try again, {name}!")
+        return summary_text
 
 
 def main():
