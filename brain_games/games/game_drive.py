7aaee1666207ebab3583ab3ec7c9eba3660e25e7
mesto3 <mesto3@gmail.com>
2023-02-28 17:55:49 +0700

    Refactor: Restyle 'task_text' printer

--- brain_games/games/game_drive.py
+++ brain_games/games/game_drive.py
@@ -9,18 +9,21 @@ from brain_games.games.progression_logic import logic_outputs as prog_logic
 
 
 def game_drive(game):
+    # intro
     print('Welcome to the Brain Games!')
     name = welcome_user()
     MAX_RAUND = 3  # max raund
+    # round revolver
     for raund_index in range(1, MAX_RAUND + 1):
-        logic_outputs_module = game + '()'
-        logic_outputs_list = eval(logic_outputs_module)
+        logic_outputs_module = game + '()'  # build logic 
+        logic_outputs_list = eval(logic_outputs_module)  # get logic results
         quest = logic_outputs_list[0]
         correct_answer = logic_outputs_list[1]
         task_text = logic_outputs_list[2]
-        if raund_index == 1:
-            print(f'{task_text}')
+        # task printer
+        print(task_text) if raund_index == 1 else None
         raund_index += 1
+        # ask quest get ans print res
         print(f'Question: {quest}')
         user_answer = input('Your answer: ')
         if user_answer == correct_answer:
@@ -31,7 +34,6 @@ def game_drive(game):
             raund_index = 0
             break
     summary(raund_index, MAX_RAUND, name)  # Print game summary
-    return raund_index, MAX_RAUND
 
 
 def main():
