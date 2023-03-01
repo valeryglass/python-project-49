53b82a15520c72290e94c074ee24c68573c4653f
Your Name <mesto3@gmailcom>
2023-03-01 22:56:05 +0700

    Refactor: Extract print_task_text from game_drive

--- brain_games/games/game_drive.py
+++ brain_games/games/game_drive.py
@@ -8,22 +8,26 @@ from brain_games.games.prime_logic import logic_outputs as prime_logic
 from brain_games.games.progression_logic import logic_outputs as prog_logic
 
 
+def print_task_text(task_text, raund_index):
+    '''Print text of task'''
+    print(task_text) if raund_index == 1 else None
+
+
 def game_drive(game):
+    '''   '''
     # intro
     print('Welcome to the Brain Games!')
     name = welcome_user()
     MAX_RAUND = 3  # max raund
     # round revolver
     for raund_index in range(1, MAX_RAUND + 1):
-        logic_outputs_module = game + '()'  # build logic 
+        logic_outputs_module = game + '()'  # build logic
         logic_outputs_list = eval(logic_outputs_module)  # get logic results
         quest = logic_outputs_list[0]
         correct_answer = logic_outputs_list[1]
         task_text = logic_outputs_list[2]
-        # task printer
-        print(task_text) if raund_index == 1 else None
+        print_task_text(task_text, raund_index)
         raund_index += 1
-        # ask quest get ans print res
         print(f'Question: {quest}')
         user_answer = input('Your answer: ')
         if user_answer == correct_answer:
